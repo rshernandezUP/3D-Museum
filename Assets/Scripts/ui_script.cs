5a63bb6d16ac8092f06e341c7c7bf6ab585cefe7
Raúl Sánchez <rshernandez@up.edu.mx>
2026-03-11 18:38:38 -0600

    Debug Adios

--- Assets/Scripts/ui_script.cs
+++ Assets/Scripts/ui_script.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class ui_script : MonoBehaviour
 {
+
+    public float value = 1f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("Adios");
     }
 
     // Update is called once per frame
